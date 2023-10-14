b67914f5ee71160fbcda8d252a94c6e06e3e67d2
bnvent <brandonventurino@gmail.com>
2023-10-14 03:26:53 -0300

    Improved fullscreen feature

--- scripts/full-screen.js
+++ scripts/full-screen.js
@@ -1,26 +1,48 @@
 const fullscreenButton = document.getElementById("fullscreen-button");
 const catalogContainer = document.getElementById("catalog-container");
+const buttonsContainer = document.getElementById("buttons-container");
+const appElement = document.getElementById("app");
 
-let enabledFullscreen = false;
+const catalogContainerStyle = catalogContainer.style;
+const buttonsContainerStyle = buttonsContainer.style;
+
+let activeFullscreen = false;
 
 const enterFullscreenHandler = () => {
-  enabledFullscreen = true;
-  catalogContainer.requestFullscreen();
+  activeFullscreen = true;
   fullscreenButton.src = "./icons/exit-fullscreen-icon.svg";
+
+  // Style changes
+  buttonsContainer.style.position = "fixed";
+  buttonsContainer.style.bottom = 0;
+  buttonsContainer.style.right = "1rem";
+
+  catalogContainer.style.maxHeight = "100%";
+  catalogContainer.style.overflow = "visible";
+
+  appElement.style.padding = 0;
+
+  // Hide elements
+  document.querySelector("header").style.display = "none";
+  document.getElementById("add-banner-accordion").style.display = "none";
 };
 
 const exitFullscreenHandler = () => {
-  enabledFullscreen = false;
-  document.exitFullscreen();
+  activeFullscreen = false;
   fullscreenButton.src = "./icons/enter-fullscreen-icon.svg";
-  return;
-};
 
-document.onfullscreenchange = () => {
-  if (!window.document.fullscreenElement) exitFullscreenHandler();
+  // Style changes
+  catalogContainer.style = catalogContainerStyle;
+  buttonsContainer.style = buttonsContainerStyle;
+  appElement.style.padding = "1rem 1.5rem 4rem 1.5rem";
+
+  // Show elements
+  document.querySelector("header").style.display = "block";
+  document.getElementById("add-banner-accordion").style.display = "flex";
 };
 
 fullscreenButton.onclick = () => {
-  if (enabledFullscreen) exitFullscreenHandler;
+  if (activeFullscreen) exitFullscreenHandler();
   else enterFullscreenHandler();
+  window.scrollTo(0, 0);
 };
